01d085b78779950ddb3d603c0eb73984899d81ec
Christoph Lukas <lukas@pingworks.de>
2013-09-25 00:38:01 +0200

    Fix

--- scripts/repo/install_bundle.sh
+++ scripts/repo/install_bundle.sh
@@ -24,7 +24,7 @@ getBundleFolder $BUNDLE
 echo "Installing bundle $BUNDLE on environment $ENV .."
 ARTIFACTS=$(cd ${BUNDLE_FOLDER}/artifacts/; ls )
 cd ${BUNDLE_FOLDER}/artifacts/ 
-scp ${ARTIFACTS} ${ENV_USER}@${ENV}:${ENV_TMP}/
+scp -o StrictHostKeyChecking=no ${ARTIFACTS} ${ENV_USER}@${ENV}:${ENV_TMP}/
 for pkg in ${ARTIFACTS}; do
   ssh -o StrictHostKeyChecking=no ${ENV_USER}@${ENV} "cd ${ENV_TMP}; ${PKGMGR} ${PKGMGR_INSTALLOPTS} $pkg"
 done 
