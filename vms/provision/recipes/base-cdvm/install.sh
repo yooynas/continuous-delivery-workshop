03a2a777448a828b881427551790938bdbd2146f
Alexander Birk <birk@pingworks.de>
2013-09-24 23:19:51 +0200

    hosts immer ersetzen

--- vms/provision/recipes/base-cdvm/install.sh
+++ vms/provision/recipes/base-cdvm/install.sh
@@ -12,12 +12,10 @@ echo "# -------------------------------------------------"
 echo "# BEGIN Provisioning RECIPE $RECIPE"
 echo "# -------------------------------------------------"
 
-TESTHOSTNAME="${DEPLOY_ENV_DEPLOY_HOST_FQDN}"
 echo "Checking /etc/hosts..."
-if [ -z "$(cat /etc/hosts | grep ${TESTHOSTNAME})" ];then
-    echo "Installing vm specific /etc/hosts file..."
-    cpAndSubstDeployEnvVars $CONF_PHOME/recipes/$RECIPE/etc_hosts /etc/hosts root.root 644
-fi
+
+echo "Installing vm specific /etc/hosts file..."
+cpAndSubstDeployEnvVars $CONF_PHOME/recipes/$RECIPE/etc_hosts /etc/hosts root.root 644
 
 echo "# -------------------------------------------------"
 echo "# END Provisioning RECIPE $RECIPE"
