72097042bae5a694cbc0c4b6fa3573ad4f42ea4f
Alexander Birk <birk@pingworks.de>
2013-09-25 01:53:03 +0200

    Eclipse Monster Bundle download & install

--- vms/provision/recipes/ide-eclipse/install.sh
+++ vms/provision/recipes/ide-eclipse/install.sh
@@ -13,16 +13,22 @@ RECIPE='ide-eclipse'
 echo "# -------------------------------------------------"
 echo "# BEGIN Provisioning RECIPE $RECIPE"
 echo "# -------------------------------------------------"
-[ ! -d /home/$CONF_DUSER/Downloads ] && mkdir /home/$CONF_DUSER/Downloads 
-if [ ! -f /home/$CONF_DUSER/Downloads/eclipse-jee.tar.gz ];then
-wget -q -O /home/$CONF_DUSER/Downloads/eclipse-jee.tar.gz ${PROVISIONING_SERVER_URI}/eclipse-jee.tar.gz > /dev/null
-  chown $CONF_DUSER.users /home/$CONF_DUSER/Downloads/eclipse-jee.tar.gz
+
+ECLIPSE_BUNDLE="eclipse-full-bundle.tar"
+
+install -o ${CONF_DUSER} -g ${CONF_DGROUP} -m 755 -d /home/$CONF_DUSER/Downloads
+
+[ ! -z "$(file /home/$CONF_DUSER/Downloads/${ECLIPSE_BUNDLE} | grep empty)" ] && rm /home/$CONF_DUSER/Downloads/${ECLIPSE_BUNDLE}
+  
+if [ ! -f /home/$CONF_DUSER/Downloads/${ECLIPSE_BUNDLE} ];then
+  echo "Downloading Monster 615MB eclipse Bundle from ${PROVISIONING_SERVER_URI}/${ECLIPSE_BUNDLE}..."
+  wget -q -O /home/$CONF_DUSER/Downloads/${ECLIPSE_BUNDLE} ${PROVISIONING_SERVER_URI}/${ECLIPSE_BUNDLE} > /dev/null
+  chown $CONF_DUSER.users /home/$CONF_DUSER/Downloads/${ECLIPSE_BUNDLE}
 fi
 
 if [ ! -d /home/$CONF_DUSER/apps/eclipse ];then
-  cd /home/$CONF_DUSER/apps/
-  tar xfz /home/$CONF_DUSER/Downloads/eclipse-jee.tar.gz
-  chown -R $CONF_DUSER.users /home/$CONF_DUSER/apps/eclipse
+  cd /home/$CONF_DUSER
+  sudo -u $CONF_DUSER tar xfz /home/$CONF_DUSER/Downloads/${ECLIPSE_BUNDLE}
 fi
 
 ECLIPSE_LAUNCHES_DIR=/home/$CONF_DUSER/workspace/.metadata/.plugins/org.eclipse.debug.core/.launches
